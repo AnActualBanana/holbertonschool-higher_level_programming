614f70c237e681bafd07fc43297dca7b0113c2b9
AnActualBanana <loganmcclain59@gmail.com>
2022-05-03 11:44:12 -0700

    added negative condition

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -7,5 +7,9 @@ if int(str(number)[-1]) == 0:
     statement = "0"
 if int(str(number)[-1]) < 6 and int(str(number)[-1]) > 0:
     statement = "less than 6 and not 0"
-print("Last digit of {} is {} and is {}".format(str(number), str(number)[-1], \
-                                                statement))
+if str(number)[0] == "-":
+    print("Last digit of {} is -{} and is {}".format(str(number), \
+                                                    str(number)[-1], statement))
+else:
+    print("Last digit of {} is {} and is {}".format(str(number), \
+                                            str(number)[-1], statement))
