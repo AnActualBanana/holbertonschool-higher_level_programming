eed858c4a2cde0b6a46d891dec93e3922e1c9649
AnActualBanana <loganmcclain59@gmail.com>
2022-05-03 11:48:13 -0700

    corrected negative case

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if int(str(number)[-1]) > 5:
+if abs(int(str(number)[-1])) > 5:
     statement = "greater than 5"
-if int(str(number)[-1]) == 0:
+if abs(int(str(number)[-1])) == 0:
     statement = "0"
-if int(str(number)[-1]) < 6 and int(str(number)[-1]) > 0:
+if abs(int(str(number)[-1])) < 6 and abs(int(str(number)[-1])) > 0:
     statement = "less than 6 and not 0"
 if str(number)[0] == "-":
     print("Last digit of {} is -{} and is {}".format(str(number), \
                                                     str(number)[-1], statement))
 else:
     print("Last digit of {} is {} and is {}".format(str(number), \
-                                            str(number)[-1], statement))
+                                                    str(number)[-1], statement))
