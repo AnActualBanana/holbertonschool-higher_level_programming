e291f077478e077792872186fb804ecdc0fa7d3b
AnActualBanana <loganmcclain59@gmail.com>
2022-05-03 12:16:12 -0700

    rework conditioons

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+if str(number)[0] == "-":
+    statement = "less than 6 and not 0"
+    print("Last digit of {} is -{} and is {}".format(str(number), \
+                                                    str(number)[-1], statement))
 if int(str(number)[-1]) > 5:
     statement = "greater than 5"
 if int(str(number)[-1]) == 0:
     statement = "0"
 if int(str(number)[-1]) < 6 and int(str(number)[-1]) > 0:
     statement = "less than 6 and not 0"
-if str(number)[0] == "-":
-    print("Last digit of {} is -{} and is {}".format(str(number), \
-                                                    str(number)[-1], statement))
-else:
+if str(number)[0] != "-":
     print("Last digit of {} is {} and is {}".format(str(number), \
                                                     str(number)[-1], statement))
