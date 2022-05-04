069c8e7c6dc0865b742738807323b2660ad706a9
AnActualBanana <loganmcclain59@gmail.com>
2022-05-04 12:26:43 -0700

    exception handling, typo fix

--- 0x01-python-import_modules/3-safe_print_division.py
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         quotient = a / b
     except:
-        quotient = "none"
+        quotient = "None"
         raise ValueError("Divide by 0 error")
     finally:
         print("Inside result: {}".format(quotient))
