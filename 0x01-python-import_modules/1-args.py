5306f722620ea7b3118d6342096fc7fa07229089
AnActualBanana <loganmcclain59@gmail.com>
2022-05-04 11:51:36 -0700

    task 1

---
+++ 0x01-python-import_modules/1-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    print("{} ".format(len(argv) - 1) ,end='')
+    if len(argv) - 1 == 1:
+        print("argument", end='')
+    else:
+        print("arguments", end='')
+    if len(argv) - 1 == 0:
+        print(".")
+    else:
+        print(":")
+        for arg in range(1, len(argv)):
+            print("{}: {}".format(arg, argv[arg]))
