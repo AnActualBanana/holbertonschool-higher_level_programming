0148a6725efdc3b1db4324fe1d1ef5dd5f05069b
AnActualBanana <loganmcclain59@gmail.com>
2022-05-04 12:13:06 -0700

    send exception to stderr

--- 0x01-python-import_modules/3-safe_print_division.py
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+import sys
+
 def safe_print_division(a, b):
     try:
         quotient = a / b
     except:
         quotient = "none"
         raise Exception("Divide by 0 error")
+        print("Divide by 0 error", file = sys.stderr)
     finally:
         print("Inside result: {}".format(quotient))
         return(quotient)
