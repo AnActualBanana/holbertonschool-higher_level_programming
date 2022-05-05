737d980cded5e60bc0b8adf47a03f12dedc9b3d3
AnActualBanana <loganmcclain59@gmail.com>
2022-05-05 09:04:29 -0700

    task 8

---
+++ 0x02-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    new_tuple = (len(sentence), sentence[0])
+    if len(sentence) == 0:
+        new_tuple = (new_tuple[0], None)
+    return(new_tuple)
