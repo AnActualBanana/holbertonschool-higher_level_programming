0feffb6cf9e221b102f2e09d45fe6185b2454236
AnActualBanana <loganmcclain59@gmail.com>
2022-05-10 10:50:13 -0700

    task 1 documentation fix

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 "A class that defines a square"
 
+
 class Square:
     "the square class, containing size and instantiation"
     def __init__(self, size=0):
-        if isinstance(size, int) != True:
+        if isinstance(size, int) is not True:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
