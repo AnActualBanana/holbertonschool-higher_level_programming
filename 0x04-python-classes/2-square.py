26a5d7839d63f08fac3652717f3da2998ed72b36
AnActualBanana <loganmcclain59@gmail.com>
2022-05-10 10:56:54 -0700

    task 2 doc fix

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 "a class that defines a square"
 
+
 class Square:
     "defines a square with size, instantiation, and public instance method"
     def __init__(self, size=0):
-        if isinstance(size, int) == False:
+        if isinstance(size, int) is False:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         return(self.__size * self.__size)
