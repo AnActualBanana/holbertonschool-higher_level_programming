a5a2a9cdfa8633aba9822f1088b251d75289c7ae
AnActualBanana <loganmcclain59@gmail.com>
2022-05-10 10:27:56 -0700

    trying to document

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""Write a class "Square" that defines a square"""
+# 0-square.py
+"""Write a class "Square" that defines a square."""
 
 
 class Square:
 """a class that defines a square"""
     def __init__(self, size):
-        """initializes a square with a size"""
+        """initializes a square with a size
+
+        Args: size (int): The size of the square.
+        """
         self.__size = size
