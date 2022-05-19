e3df0d680f6761b1cda497e3ee26f9f6999e0d07
AnActualBanana <loganmcclain59@gmail.com>
2022-05-19 10:48:08 -0700

    task 8:

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""A Rectangle class, that inherits from BaseGeometry, that's instantiated with\
- width and height"""
+"""A Rectangle class, that inherits from BaseGeometry, that's instantiated/
+ with width and height"""
 
 
 class BaseGeometry:
@@ -8,12 +8,14 @@ class BaseGeometry:
     def area(self):
         """not currently implemented"""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """checks if value is an int, and if its positive"""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
 
 class Rectangle(BaseGeometry):
     """The Rectangle class"""
