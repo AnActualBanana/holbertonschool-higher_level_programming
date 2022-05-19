c0241e41ab4eaef23ca00ab8341a9746c8b80467
AnActualBanana <loganmcclain59@gmail.com>
2022-05-19 10:21:14 -0700

    task 3 PEP8 correction

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,6 +2,7 @@
 """checks if the object is an instance of or instance of subclass of specified \
 class"""
 
+
 def is_kind_of_class(obj, a_class):
     """Returns True if the object is an instance of or is an instance of class\
 that inherited from, the specified class, otherwise returns False"""
