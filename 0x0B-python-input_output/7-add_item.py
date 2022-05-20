3adf46871b957db5d07cfb7890018531bf63ecb3
AnActualBanana <loganmcclain59@gmail.com>
2022-05-20 09:50:01 -0700

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""adds all arguments to a python list, and saves them to a file"""
+
+
+import json
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+def main(*argv):
+    """makes a list of arguments, serializes them, then makes a new object"""
+    if len(sys.argv) == 1:
+        return
+    try:
+        arg_list = load_from_json_file("add_item.json")
+    except:
+        arg_list = []
+    for arg in range(1, len(sys.argv)):
+        arg_list.append(sys.argv[arg])
+    save_to_json_file(arg_list, "add_item.json")
+main()
