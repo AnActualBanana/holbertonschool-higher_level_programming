3b550fb13cd58be2fd4dfb4508f021cf1fea2964
AnActualBanana <loganmcclain59@gmail.com>
2022-05-20 11:39:07 -0700

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""a student class that has name, age, and a method to retrieve\
+a dictionary representation of a Stduent instance"""
+
+
+class Student:
+    """class with name and age, and a method that returns a dict\
+representation of itself"""
+    def __init__(self, first_name, last_name, age):
+        """instantiation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves dict representation of attributes specified,\
+ or all attributes if none are specified"""
+        if attrs is not None:
+            old_dict = self.__dict__
+            new_dict = dict()
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+                for key in [*old_dict]:
+                    if key == attr:
+                        new_dict[key] = old_dict[key]
+            return new_dict
+        return self.__dict__
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,9 +7,11 @@ class Student:
     """class with name and age, and a method that returns a dict\
 representation of itself"""
     def __init__(self, first_name, last_name, age):
+        """instantiation"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """returns dictionary representation"""
         return self.__dict__
