8bb10e4f7c6f34d27a470728bad3ad5dc972975a
AnActualBanana <loganmcclain59@gmail.com>
2022-05-20 08:03:43 -0700

    task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""returns an object represented by a JSON string"""
+
+
+import json
+
+def from_json_string(my_str):
+    """converts JSON string into a dict(python obj) and returns it"""
+    my_obj = json.loads(my_str)
+    return my_obj
