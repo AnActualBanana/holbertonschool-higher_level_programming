5226c67abaafc3f9b4b5d3da3587f5d416e5f511
AnActualBanana <loganmcclain59@gmail.com>
2022-05-20 07:54:27 -0700

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""returns the JSON represenation of an object"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """serializes my_obj"""
+    my_obj_json = json.dumps(my_obj)
+    return my_obj_json
