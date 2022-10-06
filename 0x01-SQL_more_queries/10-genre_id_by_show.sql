e5514cde7afdb7ab6affa9e1273f9d27155514c0
AnActualBanana <loganmcclain59@gmail.com>
2022-10-06 08:39:14 -0700

    remaining tasks

---
+++ 0x01-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- Lists all shows that have a genre
+-- Sorted by ascending title and id
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+        INNER JOIN `tv_show_genres` AS g
+	ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
---
+++ 0x01-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- Lists all shows
+-- Displays NULL for shows without genres.
+-- Sorted by ascending title and id
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
