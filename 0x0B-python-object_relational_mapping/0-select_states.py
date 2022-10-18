03f9f0b3ca4644ae59f24987a3432bb137a2c8ec
AnActualBanana <loganmcclain59@gmail.com>
2022-10-18 09:02:19 -0700

    rework 0

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
 """
-lists all states from the database hbtn_0e_0_usa
+Script that will list all states
 """
-
 import MySQLdb
 
 
-def print_state():
+if __name__ == "__main__":
     from sys import argv
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], database=argv[3])
+    dbconnect = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                             passwd=argv[2], database=argv[3])
 
-    cursor = db.cursor()
+    cursor = dbconnect.cursor()
 
     cursor.execute("SELECT * FROM states")
-    for rows in cursor.fetchall():
-        print(rows)
+    for row in cursor.fetchall():
+        print(row)
 
     cursor.close()
-    db.close()
-
-
-if __name__ == "__main__":
-    print_state()
+    dbconnect.close()
