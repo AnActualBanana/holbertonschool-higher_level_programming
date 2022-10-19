d325df0b1397a7da7ef5ff2fe5184460db081ddb
AnActualBanana <loganmcclain59@gmail.com>
2022-10-19 10:35:21 -0700

    pylint

--- 0x0B-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0B-python-object_relational_mapping/4-cities_by_state.py
@@ -7,11 +7,13 @@ import MySQLdb
 
 if __name__ == "__main__":
     from sys import argv
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], database=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], database=argv[3])
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id")
+    cursor.execute("SELECT cities.id, cities.name,\
+ states.name FROM cities JOIN states ON cities.state_id = states.id")
     for row in cursor.fetchall():
         print(row)
 
