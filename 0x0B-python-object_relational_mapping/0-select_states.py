d366b13b9b056bced3b795547e09bed19ccc4d2c
AnActualBanana <loganmcclain59@gmail.com>
2022-10-19 08:56:54 -0700

    error 2006

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -7,14 +7,13 @@ import MySQLdb
 
 if __name__ == "__main__":
     from sys import argv
-    dbconnect = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                             passwd=argv[2], database=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], database=argv[3])
 
-    cursor = dbconnect.cursor()
+    cursor = db.cursor()
 
     cursor.execute("SELECT * FROM states")
     for row in cursor.fetchall():
         print(row)
 
-    cursor.close()
-    dbconnect.close()
+        cursor.close()
+        db.close()
