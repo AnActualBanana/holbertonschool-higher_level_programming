50183d99abf06a08d423046bbeae7ed0b5d2d041
AnActualBanana <loganmcclain59@gmail.com>
2022-10-19 10:05:32 -0700

    task 3

---
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Script that displays all values in databSe where name matches argument
+"""
+import MySQLdb
+
+if __name__ == "__main__":
+    from sys import argv
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], database=argv[3])
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s", (argv[4],))
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
