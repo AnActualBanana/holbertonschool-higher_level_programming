2be3d4a1b8e8c4885ca7625d64ca85bb23040de8
AnActualBanana <loganmcclain59@gmail.com>
2022-10-20 07:27:02 -0700

    task 7

---
+++ 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+script that lists all states
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    db = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                       .format(argv[1], argv[2], arg[3]),
+                       pool_pre_ping=True
+
+                       Base.metadata.create_all(db)
+                       sesh = sessionmaker(bind=db)
+
+                       for row in dbsession().query(State.order_by(State.id):
+                                                    print("{}: {}".format(row.id, row.name))
+                                                    sesh().close
