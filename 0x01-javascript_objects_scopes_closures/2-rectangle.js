0607b67adac06b4a37a3c488b97bdac03f6e2679
AnActualBanana <loganmcclain59@gmail.com>
2022-12-02 06:53:38 -0800

    semistandard on task 1, and task 2

--- 0x01-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,6 +1,11 @@
-#!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
+    if (!w || !h) {
+      return;
+    }
+    if (w <= 0 || h <= 0) {
+      return;
+    }
     this.width = w;
     this.height = h;
   }
